1c5a75a1709f8eb1380291d53522e07f1014c8a4
YGM <83952174+rjsah09@users.noreply.github.com>
2023-03-13 21:17:41 +0900

    [Silver III] Title: N과 M (4), Time: 212 ms, Memory: 19860 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static int[] list;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        StringTokenizer st = new StringTokenizer(input);
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        list = new int[m];
+
+        for(int i = 0; i < n; i++) {
+            dfs(i, 0);
+        }
+
+        System.out.println(sb);
+    }
+
+    static void dfs(int node, int depth) {
+        list[depth++] = node + 1;
+
+        if(depth == m) {
+            for(int i = 0; i < m; i++)
+                sb.append(list[i] + " ");
+            sb.append("\n");
+            return;
+        }
+
+        for(int i = node; i < n; i++) {
+            dfs(i, depth);
+        }
+    }
+}
