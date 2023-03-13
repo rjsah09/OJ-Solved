c5e3a4648c3470055167e45fa42d7572e0fb6cb9
YGM <83952174+rjsah09@users.noreply.github.com>
2023-03-13 20:54:44 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner out = new Scanner(System.in);
+        int a = out.nextInt();
+        int b = out.nextInt();
+        System.out.println(a + b);
+    }
+}
