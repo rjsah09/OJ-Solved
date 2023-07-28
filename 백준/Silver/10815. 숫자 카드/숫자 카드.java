6a1714286c06c76030c2499a873198d7637878aa
YGM <83952174+rjsah09@users.noreply.github.com>
2023-07-28 16:59:40 +0900

    [Silver V] Title: 숫자 카드, Time: 1024 ms, Memory: 150880 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,7 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -9,39 +10,22 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        int[] cards = new int[N];
+        Set<Integer> cardSet = new HashSet<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            cards[i] = Integer.parseInt(st.nextToken());
+            cardSet.add(Integer.parseInt(st.nextToken()));
         }
-        Arrays.sort(cards);
 
         int M = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
             int target = Integer.parseInt(st.nextToken());
-            sb.append(binarySearch(cards, target) + " ");
+            int found = cardSet.contains(target) ? 1 : 0;
+            sb.append(found + " ");
         }
 
         System.out.println(sb);
     }
 
-    static int binarySearch(int[] cards, int target) {
-        int lowIndex = 0;
-        int highIndex = cards.length - 1;
-
-        while (lowIndex <= highIndex) {
-            int centerIndex = (lowIndex + highIndex) / 2;
-            if (cards[centerIndex] == target) {
-                return 1;
-            } else if (cards[centerIndex] < target) {
-                lowIndex = centerIndex + 1;
-            } else {
-                highIndex = centerIndex - 1;
-            }
-        }
-
-        return 0;
-    }
 }
