d9181f0a23c64b951a6b81f139b534c068de8825
YGM <83952174+rjsah09@users.noreply.github.com>
2023-09-02 22:12:30 +0900

    [Silver III] Title: 크면서 작은 수, Time: 184 ms, Memory: 16688 KB -BaekjoonHub

---
+++ 백준/Silver/2992. 크면서 작은 수/크면서 작은 수.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static String X;
+    static boolean[] visited;
+    static char[] XValue;
+
+    static int result;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        X = br.readLine();
+
+        result = 0;
+        visited = new boolean[X.length()];
+        XValue = new char[X.length()];
+        for (int i = 0; i < XValue.length; i++) {
+            XValue[i] = X.charAt(i);
+        }
+
+        for (int i = 0; i < XValue.length; i++) {
+            if (XValue[i] - 0 >= XValue[0] - 0) {
+                dfs("", i);
+            }
+        }
+
+        System.out.println(result);
+
+    }
+
+    public static void dfs(String prev, int index) {
+        visited[index] = true;
+        String now = prev + XValue[index];
+
+        if (now.length() == X.length()) {
+            int totalNum = Integer.parseInt(now);
+            if (totalNum <= Integer.parseInt(X)) {
+                visited[index] = false;
+                return;
+            }
+            result = result == 0 ? Integer.parseInt(now) : Math.min(result, Integer.parseInt(now));
+        }
+
+        for (int i = 0; i < XValue.length; i++) {
+            if (!visited[i] && Integer.parseInt(now + XValue[i]) >= Integer.parseInt(X.substring(0, prev.length() + 1))) {
+                dfs (now, i);
+            }
+        }
+
+        visited[index] = false;
+    }
+}
