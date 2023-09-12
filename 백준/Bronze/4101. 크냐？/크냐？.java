0abe89e08644c3c91c1963a5f8b6a836ea04c718
YGM <83952174+rjsah09@users.noreply.github.com>
2023-09-12 22:11:41 +0900

    [Bronze V] Title: 크냐?, Time: 124 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+
+        StringBuilder sb = new StringBuilder();
+        while (!line.equals("0 0")) {
+            StringTokenizer st = new StringTokenizer(line);
+            int left = Integer.parseInt(st.nextToken());
+            int right = Integer.parseInt(st.nextToken());
+
+            if (left > right) {
+                sb.append("Yes\n");
+            } else {
+                sb.append("No\n");
+            }
+
+            line = br.readLine();
+        }
+
+        System.out.println(sb);
+    }
+}
