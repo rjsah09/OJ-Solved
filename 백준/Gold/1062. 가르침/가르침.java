4176c6fe90d43107934c47671058764730a6abee
YGM <83952174+rjsah09@users.noreply.github.com>
2023-09-20 08:39:12 +0900

    [Gold IV] Title: 가르침, Time: 348 ms, Memory: 16252 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.java
+++ 백준/Gold/1062. 가르침/가르침.java
@@ -1,69 +1,89 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
- 
+
 public class Main {
- 
-    static int n, k;
-    static int max = Integer.MIN_VALUE;
+    static int N, K;
+    static String[] words;
     static boolean[] visited;
-    static String[] word;
-    
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
- 
-        n = scan.nextInt();
-        k = scan.nextInt();
-        
-        scan.nextLine();
-        word = new String[n];
-        for(int i = 0; i < n; i++) {
-            String str = scan.nextLine();
-            str = str.replace("anta", "");
-            str = str.replace("tica", "");
-            word[i] = str;
-        }
-        
-        if(k < 5) { //a c i n t의 개수가 5개이므로
-            System.out.println("0");
-            return;
-        } else if(k == 26) {
-            System.out.println(n);
-            return;
-        }
-        
+
+    static int result;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
         visited = new boolean[26];
-        visited['a' - 'a'] = true;
-        visited['c' - 'a'] = true;
-        visited['i' - 'a'] = true;
+
+        if (K < 5) {
+            System.out.println(0);
+            return;
+        }
+
+        result = 0;
+        words = new String[N];
+        for (int i = 0; i < N; i++) {
+            words[i] = br.readLine();
+        }
+
+        visited[0] = true;
         visited['n' - 'a'] = true;
         visited['t' - 'a'] = true;
-        
-        backtracking(0, 0);
-        System.out.println(max);
-    }
-    
-    public static void backtracking(int alpha, int len) {
-        if(len == k - 5) {
-            int count = 0;
-            for(int i = 0; i < n; i++) {
-                boolean read = true;
-                for(int j = 0; j < word[i].length(); j++) {
-                    if(!visited[word[i].charAt(j) - 'a']) {
-                        read = false;
-                        break;
-                    }
-                }
-                if(read) count++;
-            }
-            max = Math.max(max, count);
+        visited['i' - 'a'] = true;
+        visited['c' - 'a'] = true;
+
+        if (K == 5) {
+            System.out.println(getKnowingWordCount());
+            return;
+        } else if (K == 26) {
+            System.out.println(words.length);
             return;
         }
-        
-        for(int i = alpha; i < 26; i++) {
-            if(visited[i] == false) {
+
+        for (int i = 0; i < 26; i++) {
+            if (!visited[i]) {
                 visited[i] = true;
-                backtracking(i, len + 1);
+                dfs(i, 6);
                 visited[i] = false;
             }
         }
+        
+        System.out.println(result);
+    }
+
+    static void dfs(int node, int letterCount) {
+        if (letterCount == K) {
+            int knowingWordsCount = getKnowingWordCount();
+            result = Math.max(result, knowingWordsCount);
+            return;
+        }
+
+        for (int i = node; i < 26; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                dfs(i, letterCount + 1);
+                visited[i] = false;
+            }
+        }
+
+    }
+
+    static int getKnowingWordCount() {
+        int count = words.length;
+        for (int i = 0; i < words.length; i++) {  //단어
+            int knowingLetterCount = 0;
+
+            for (int j = 0; j < words[i].length(); j++) {   //단어의 한 글자
+                int index = words[i].charAt(j) - 'a';
+                if (!visited[index]) {
+                    count--;
+                    break;
+                }
+            }
+        }
+
+        return count;
     }
 }
