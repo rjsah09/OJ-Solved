843b6eca2126cef0774ea0489b902a98586eb124
YGM <83952174+rjsah09@users.noreply.github.com>
2023-11-13 20:56:39 +0900

    [Silver IV] Title: 점화식, Time: 128 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Silver/13699. 점화식/점화식.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        long[] dp = new long[N + 1];   //t(N)
+
+        dp[0] = 1L;
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i - j - 1];
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
+}
