814e330532dfdca62f82937a6bc301d49931cd5b
YGM <83952174+rjsah09@users.noreply.github.com>
2024-03-12 22:23:35 +0900

    [Silver II] Title: 지구 온난화, Time: 120 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Silver/5212. 지구 온난화/지구 온난화.java
+++ 백준/Silver/5212. 지구 온난화/지구 온난화.java
@@ -1,95 +1,72 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static String[][] board;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int R = Integer.parseInt(st.nextToken());   //세로 크기
-        int C = Integer.parseInt(st.nextToken());   //가로 크기
-        Queue<int[]> floodQueue = new LinkedList<>();
+        int R = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
 
-        //보드 채우기
-        board = new String[R][C];
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, 1, 0, -1};
+
+        String[][] map = new String[R][C];  //맵
+        String[][] resultMap = new String[R][C];    //결과맵
+
+        // 입력 받기
         for (int i = 0; i < R; i++) {
-            board[i] = br.readLine().split("");
+            String line = br.readLine();
+            for (int j = 0; j < C; j++) {
+                String now = String.valueOf(line.charAt(j));
+                map[i][j] = now;
+                resultMap[i][j] = now;
+            }
         }
 
-        //물에 잠기는 땅 위치 저장
+        // 50년 후의 지도 만들기
         for (int i = 0; i < R; i++) {
             for (int j = 0; j < C; j++) {
-                if (isFlood(i, j)) {
-                    floodQueue.add(new int[] {i, j});
+
+                if (map[i][j].equals("X")) {
+                    int seaCount = 0;
+                    
+                    for (int k = 0; k < 4; k++) {
+                        int nx = i + dx[k];
+                        int ny = j + dy[k];
+                        if (nx >= 0 && nx < R && ny >= 0 && ny < C && map[nx][ny].equals(".")) {
+                            seaCount++;
+                        } else if (nx < 0 || nx >= R || ny < 0 || ny >= C) {    /** 오류 */
+                            seaCount++;
+                        }
+                    }
+                    
+                    if (seaCount >= 3) {
+                        resultMap[i][j] = ".";
+                    }
                 }
             }
         }
 
-        //물에 잠기는 땅 변환
-        while (!floodQueue.isEmpty()) {
-            int[] pos = floodQueue.poll();
-            board[pos[0]][pos[1]] = ".";
-        }
-
-        //사이즈 측정
-        int[] min = new int[] {-1, -1};
-        int[] max = new int[] {-1, -1};
+        // 결과 출력
+        int startX = R, startY = C, endX = 0, endY = 0;
         for (int i = 0; i < R; i++) {
             for (int j = 0; j < C; j++) {
-                if (board[i][j].equals("X")) {
-                    min[0] = min[0] == -1 ? i : Math.min(min[0], i);
-                    min[1] = min[1] == -1 ? j : Math.min(min[1], j);
-                    max[0] = max[0] == -1 ? i : Math.max(max[0], i);
-                    max[1] = max[1] == -1 ? j : Math.max(max[1], j);
+                if (resultMap[i][j].equals("X")) {
+                    startX = Math.min(startX, i);
+                    startY = Math.min(startY, j);
+                    endX = Math.max(endX, i);
+                    endY = Math.max(endY, j);
                 }
             }
         }
-
-        /*for (int i = 0; i < R; i++) {
-            System.out.println(String.join("", board[i]));
-        }*/
-
-        //System.out.println(min[0] + ", " + min[1] + "부터 " + max[0] + ", " + max[1] + "까지");
-
-        //결과 생성
-        StringBuilder sb = new StringBuilder();
-        for (int i = min[0]; i <= max[0]; i++) {
-            sb.append(String.join("", board[i]), min[1], max[1] + 1).append("\n");
-        }
-
-        System.out.println(sb);
-    }
-
-    static boolean isFlood(int row, int col) {
-        int[] dx = new int[]{-1, 0, 1, 0};
-        int[] dy = new int[]{0, -1, 0, 1};
-
-        int waterCount = 0;
-        for (int i = 0; i < 4; i++) {
-            if (!isIndexSafe(row + dx[i], col + dy[i])) {
-                waterCount++;
-                continue;
+        for (int i = startX; i <= endX; i++) {
+            for (int j = startY; j <= endY; j++) {
+                System.out.print(resultMap[i][j]);
             }
-
-            if (board[row + dx[i]][col + dy[i]].equals(".")) {
-                waterCount++;
-            }
+            System.out.println();
         }
-
-        return waterCount >= 3;
-    }
-
-    static boolean isIndexSafe(int row, int col) {
-        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length) {
-            return false;
-        }
-
-        return true;
     }
 }
