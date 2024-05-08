b1b200d5bae2a8363d3173246c01445ee68c3feb
YGM <83952174+rjsah09@users.noreply.github.com>
2024-05-08 15:54:52 +0900

    [Silver I] Title: 징검다리 건너기 (small), Time: 192 ms, Memory: 14856 KB -BaekjoonHub

--- 백준/Silver/22869. 징검다리 건너기 （small）/징검다리 건너기 （small）.java
+++ 백준/Silver/22869. 징검다리 건너기 （small）/징검다리 건너기 （small）.java
@@ -4,46 +4,65 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static int K;
-    static int[] powers;
-    static boolean[] dp;
+    static int N;   //돌의 개수
+    static int K;   //최대 낼 수 있는 힘
+    static int[] powers;    //N번째 돌에서 요구하는 힘(Ai)
+    static boolean[] dp;    //N번째 돌을 건너갈 수 있는지 저장하기 위한 배열
 
     public static void main(String[] args) throws IOException {
+        //값 초기화 시작
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
         powers = new int[N];
+        dp = new boolean[N];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             powers[i] = Integer.parseInt(st.nextToken());
         }
 
-        dp = new boolean[N];  //가장 많은 잔여 에너지
         dp[0] = true;
+        //값 초기화 끝
 
-        for (int i = 1; i < N; i++) {
+        /*//dp 계산 시작
+        for (int i = 1; i < N; i++) {   //도착점
             for (int j = 0; j < i; j++) {   //시작점
-                if (!dp[j]) {
+                if (!dp[j]) {   //시작점이 도착 불가능한 지역이라면 계산할 필요가 없으므로 continue
                     continue;
                 }
-                
-                int required = getRequiredPower(j, i);
-                if (required <= K) {
+
+                int required = getRequiredPower(j, i);  //j에서 i로 가는 비용을 계산하고 required 변수에 저장한다
+
+                if (required <= K) {    //비용이 K보다 작은 경우 건널 수 있다는 뜻이므로 dp[i]를 true로 갱신한다
                     dp[i] = true;
                 }
             }
         }
+        //dp 계산 끝*/
 
-        /*for (int i = 0; i < N; i++) {
-            System.out.println(dp[i]);
-        }*/
+        //2안
+        for (int i = 0; i < N; i++) {  //시작점
+            if (!dp[i]) {
+                continue;
+            }
+
+            for (int j = i + 1; j < N; j++) {   //끝점
+                int required = getRequiredPower(i, j);  //j에서 i로 가는 비용을 계산하고 required 변수에 저장한다
+
+                if (required <= K) {    //비용이 K보다 작은 경우 건널 수 있다는 뜻이므로 dp[i]를 true로 갱신한다
+                    dp[j] = true;
+                }
+            }
+        }
+
+        //dp[N -1]의 값에 따라 출력할 문구를 선택한다
         String result = dp[N - 1] ? "YES" : "NO";
         System.out.println(result);
     }
 
+    //start에서 end로 가는 비용을 반환하는 메서드
     static int getRequiredPower(int start, int end) {
         return (end - start) * (1 + Math.abs(powers[start] - powers[end]));
     }
