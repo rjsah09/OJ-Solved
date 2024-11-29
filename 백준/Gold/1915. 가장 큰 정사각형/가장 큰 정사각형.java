e5015e98157ee10341e6190af891d5e080d64576
YGM <83952174+rjsah09@users.noreply.github.com>
2024-11-29 21:45:00 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 280 ms, Memory: 25376 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
@@ -5,44 +5,34 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int[][] board;
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            char[][] grid;
+            int[][] dp;
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        board = new int[N][M];
-        int answer = 0;
-
-        for (int i = 0; i < N; i++) {
-            String line = br.readLine();
-            for (int j = 0; j < M; j++) {
-                board[i][j] = Integer.parseInt(line.substring(j, j + 1));
-                if (board[i][j] == 1) {
-                    answer = 1;
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            grid = new char[n+1][m+1];
+            for (int i = 1; i <=n; i++) {
+                String line = br.readLine();
+                for (int j=1; j <= m; j++) {
+                    grid[i][j] = line.charAt(j-1);
                 }
             }
-        }
 
-        for (int i = 1; i < N; i++) {
-            for (int j = 1; j < M; j++) {
-                if (board[i][j] > 0) {
-                    int maxLength;
-
-                    if (board[i -1][j] == board[i][j - 1] && board[i - 1][j - 1] < board[i - 1][j]) {
-                        maxLength = board[i - 1][j - 1] + 1;
-                    } else {
-                        maxLength = Math.min(board[i -1][j], board[i][j - 1]) + 1;
+            int ans = 0;
+            dp = new int[n+1][m+1];
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= m; j++) {
+                    if(grid[i][j]=='1') {
+                        int cell = Math.min(dp[i-1][j-1], Math.min(dp[i][j-1], dp[i-1][j])) + 1;
+                        dp[i][j] = cell;
+                        if (ans < cell) ans = cell;
                     }
-
-                    board[i][j] = maxLength;
                 }
-
-                answer = Math.max(answer, board[i][j]);
             }
-        }
 
-        System.out.println(answer * answer);
-    }
+            System.out.println(ans*ans);
+        }
 }
